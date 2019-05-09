5fc4c3f065612336b7ef932440fedce63c3da344
nahtechnology <antonio.alonso@nahtechnology.com>
2019-05-09 11:51:22 -0500

    Se agrega la entidad de catalogo

---
+++ candy-nucleo/src/main/java/com/mx/candy/nucleo/entidad/CatalogoEntidad.java
@@ -0,0 +1,37 @@
+package com.mx.candy.nucleo.entidad;
+
+import javax.persistence.Basic;
+import javax.persistence.Column;
+import javax.persistence.Id;
+import javax.persistence.MappedSuperclass;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+@MappedSuperclass
+public class CatalogoEntidad {
+
+    private Short clave;
+    private String descripcion;
+
+    @Id
+    @Column(name = "clave")
+    public Short getClave() {
+        return clave;
+    }
+
+    public void setClave(Short clave) {
+        this.clave = clave;
+    }
+
+    @Basic
+    @Column(name = "descripcion")
+    @NotNull
+    @Size(min = 3, max = 30)
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+}
